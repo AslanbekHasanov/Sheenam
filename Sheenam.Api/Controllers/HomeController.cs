2850c681d176f524f57bf989c9fc36dd1ece2517
A S L A N B E K <learneraslanbek1220@gmail.com>
2023-10-12 10:44:57 +0500

    INFRA: Initialize API Project

---
+++ Sheenam.Api/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using RESTFulSense.Controllers;
+
+namespace Sheenam.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class HomeController: RESTFulController
+    {
+        [HttpGet]
+        public IActionResult Get() => Ok("Hello Net");
+
+    }
+}
