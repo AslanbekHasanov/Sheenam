4c9dbb71ad51e363edae376965806c2c6c928d3b
AslanbekHasanov <learneraslanbek1220@gmail.com>
2024-06-20 17:59:27 +0500

    ShouldThrowValidationExceptionOnAddIfGuestIsNullAndLogItAsync -> WIP

---
+++ Sheenam.Api.Unit/Services/Foundations/Guests/GuestServiceTests.Logic.Add.cs
@@ -0,0 +1,43 @@
+﻿//=================================
+//Copyright(c) Coalition of Good-Hearted Engineers
+//Free To Use To Find Comfort and Peace
+//=================================
+
+using FluentAssertions;
+using Force.DeepCloner;
+using Moq;
+using Sheenam.Api.Models.Foundations.Guests;
+using Xunit;
+
+namespace Sheenam.Api.Unit.Services.Foundations.Guests
+{
+    public partial class GuestServiceTests
+    {
+        [Fact]
+        public async Task ShouldAddGuestAsync()
+        {
+            //given => kerakli ma'lumotlarni olib kelish
+            Guest randomGuest = CreateRandomGuest();
+            Guest inputGuest = randomGuest;
+            Guest storageGuest = inputGuest;
+            Guest expectedGuest = storageGuest.DeepClone();
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.InsertGuestAsync(inputGuest))
+                .ReturnsAsync(storageGuest);
+
+            //when=> kerakli methodlarni chaqirish
+            Guest actualGuest = 
+                await this.guestService.AddGuestAsync(inputGuest);
+
+            //then => tasdiqlash
+            actualGuest.Should().BeEquivalentTo(randomGuest);
+
+            this.storageBrokerMock.Verify(broker =>
+               broker.InsertGuestAsync(inputGuest),
+               Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ Sheenam.Api.Unit/Services/Foundations/Guests/GuestServiceTests.cs
@@ -0,0 +1,50 @@
+﻿//=================================
+//Copyright(c) Coalition of Good-Hearted Engineers
+//Free To Use To Find Comfort and Peace
+//=================================
+
+using FluentAssertions;
+using Moq;
+using Sheenam.Api.Brokers.Storages;
+using Sheenam.Api.Models.Foundations.Guests;
+using Sheenam.Api.Services.Foundations.Guests;
+using Tynamix.ObjectFiller;
+using Xunit;
+
+namespace Sheenam.Api.Unit.Services.Foundations.Guests
+{
+    public partial class GuestServiceTests
+    {
+        private readonly Mock<IStorageBroker> storageBrokerMock;
+        private readonly IGuestService guestService;
+
+        public GuestServiceTests()
+        {
+            this.storageBrokerMock = new Mock<IStorageBroker>();
+
+            this.guestService = 
+                new GuestService(this.storageBrokerMock.Object);
+        }
+        private DateTimeOffset GetRandomDatetimeOffset() =>
+            new DateTimeRange(earliestDate: DateTime.UnixEpoch).GetValue();
+
+        private static DateTimeOffset GetRandomDateTime() =>
+            new DateTimeRange(earliestDate: DateTime.UnixEpoch).GetValue();
+
+        private static Guest CreateRandomGuest() =>
+            CreateGuestFiller(GetRandomDateTime()).Create();
+
+        private static Guest CreateRandomJob(DateTimeOffset dates) =>
+            CreateGuestFiller(dates).Create();
+
+        private static Filler<Guest> CreateGuestFiller(DateTimeOffset dates)
+        {
+            var filler = new Filler<Guest>();
+
+            filler.Setup()
+                .OnType<DateTimeOffset>().Use(dates);
+
+            return filler;
+        }
+    }
+}
